fd790b4a670a24b8eea340d2f0d50c3ddd024231
kojima <kojima_y@contents.ne.jp>
2017-12-28 15:37:56 +0900

    rm ts

--- gulpfile.js
+++
@@ -1,8 +0,0 @@
-var gulp = require('gulp');
-var requireDir = require('require-dir');
-var config = require('./gulp/config').watch;
-
-requireDir('./gulp/tasks', {recurse: true});
-
-gulp.task('default', ['watch', 'browserSync']);
-gulp.task('y', ['watch', 'browserSync_y']);
